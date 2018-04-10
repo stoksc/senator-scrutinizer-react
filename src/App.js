f6b6ebf4a6c8b46c6daf0c92e6195dcc40a9dfe2
Joseph <richtersscales@gmail.com>
2018-04-10 14:58:43 -0400

    added fullpage scrolling

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import FrequencyGraph from './components/frequencyGraph';
 import HashtagHistogram from './components/hashtagHistogram';
 import USAMap from "react-usa-map";
 import { Media } from 'react-bootstrap';
+import { Fullpage, Slide, HorizontalSlider } from 'fullpage-react';
 import './App.css';
+
+const fullPageOptions = {
+  scrollSensitivity: 5,
+  touchSensitivity: 5,
+  scrollSpeed: 500,
+  hideScrollBars: true,
+  enableArrowKeys: true
+};
+const tweetsDetailSlidesProps = {
+  name: 'tweetsPage',
+  infinite: true,
+};
 
 class App extends Component {
   constructor(props) {
@@ -56,53 +69,69 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <div className="app">
-        <div>
-          <div className="selection">
-            <div className="usamap">
-              <USAMap onClick={this.mapHandler} />
-            </div>
-            <div className="representativeTable">
-              {this.state.stateSenators.map((representative) => {
-                return (
-                  <Media className="representativeTile" onClick={this.selectRep}>
-                    <Media.Left>
-                      <img width={64} height={64} src={representative.img} alt="thumbnail" />
-                    </Media.Left>
-                    <Media.Body>
-                      <Media.Heading>
-                        {representative.name}
-                      </Media.Heading>
-                      {representative.desc}
-                    </Media.Body>
-                  </Media>
-            )})}
-            </div>
+    const tweetsDetailSlides = [
+      <Slide>
+        <div className="tweetTable">
+          {this.state.tweets.map((tweet) => {
+            return (
+              <Media className="tweetTile">
+                <Media.Left>
+                  <img width={64} height={64} src={'https://pbs.twimg.com/profile_images/621070518834212864/ikIQKvUq_400x400.jpg'} alt="thumbnail" />
+                </Media.Left>
+                <Media.Body>
+                  <Media.Heading>
+                    {tweet.heading}
+                  </Media.Heading>
+                  {tweet.text}
+                </Media.Body>
+              </Media>
+            )
+          })}
+        </div>
+      </Slide>,
+      <Slide> Slide 2.2 </Slide>
+    ];
+    tweetsDetailSlidesProps.slides = tweetsDetailSlides;
+    const slides = [
+      <Slide>
+        <div className="selection">
+          <div className="usamap">
+            <USAMap onClick={this.mapHandler} />
           </div>
-          <div className="frequencyGraph">
-            <FrequencyGraph />
-          </div>
-          <div className="hashtagHistogram">
-            <HashtagHistogram />
-          </div>
-          <div className="tweetTable">
-            {this.state.tweets.map((tweet) => {
+          <div className="representativeTable">
+            {this.state.stateSenators.map((representative) => {
               return (
-                <Media className="tweetTile">
+                <Media className="representativeTile" onClick={this.selectRep}>
                   <Media.Left>
-                    <img width={64} height={64} src={'https://pbs.twimg.com/profile_images/621070518834212864/ikIQKvUq_400x400.jpg'} alt="thumbnail" />
+                    <img width={64} height={64} src={representative.img} alt="thumbnail" />
                   </Media.Left>
                   <Media.Body>
                     <Media.Heading>
-                      {tweet.heading}
+                      {representative.name}
                     </Media.Heading>
-                    {tweet.text}
+                    {representative.desc}
                   </Media.Body>
                 </Media>
-              )
-            })}
+          )})}
           </div>
+        </div>
+      </Slide>,
+      <Slide>
+        <div className="frequencyGraph">
+          <FrequencyGraph />
+        </div>
+        <div className="hashtagHistogram">
+          <HashtagHistogram />
+        </div>
+      </Slide>,
+      <HorizontalSlider {...tweetsDetailSlidesProps}></HorizontalSlider>
+    ];
+    fullPageOptions.slides = slides;
+
+    return (
+      <div className="app">
+        <div>
+          <Fullpage {...fullPageOptions} />
         </div>
       </div>
     );
